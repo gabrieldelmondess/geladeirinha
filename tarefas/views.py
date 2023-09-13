b681c3921199c7ab637b16b3337af44421ce5c41
gabriel <rglima2211@gmail>
2023-09-13 09:44:29 -0300

    terceiro commit

--- tarefas/views.py
+++ tarefas/views.py
@@ -7,6 +7,15 @@ class ListTodo(generics.ListAPIView):
     queryset = Todo.objects.all()
     serializer_class = ToDoSerializer
     template_name = 'todo_list.html'
+    
+    def get(self, request, *args, **kwargs):
+        todos = Todo.objects.all()
+        contexto = {
+            'caminho_para_bootstrap_css': 'https://bootswatch.com/morph/bootstrap.min.css',
+            'todos' : todos,
+        }
+        return render(request, self.template_name, contexto)
+
 
 class DetailTodo(generics.RetrieveUpdateAPIView):
     queryset = Todo.objects.all()
@@ -15,7 +24,19 @@ class DetailTodo(generics.RetrieveUpdateAPIView):
 class CreateTodo(generics.CreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = ToDoSerializer
+    template_name = "create.html"
+
+    def post( self,request, *args, **kwargs):
+        todos = Todo.objects.all()
+        contexto = {
+            'caminho_para_bootstrap_css': 'https://bootswatch.com/morph/bootstrap.min.css',
+            'todos' : todos,
+        }
+        return render(request, self.template_name, contexto)
 
 class DeleteTodo(generics.DestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = ToDoSerializer
+
+
+
