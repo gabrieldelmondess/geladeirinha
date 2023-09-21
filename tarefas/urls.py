ee8fda49a2a176e91a949200be27561a94926d4c
Gabriel Ângelo Delmondes de Lima <gabriel.dlima@dinfor.uniceub>
2023-09-21 09:05:25 -0300

    Corrigindo bugs e adicionando o primeiro redirect do app

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import *
+
+app_name = 'tarefas'
+
 urlpatterns = [
     path('<int:pk>/', DetailTodo.as_view()),
     path('', ListTodo.as_view(), name='list-todo'),
-    path('create', CreateTodo.as_view()),
+    path('create/', CreateTodo.as_view(), name='create_todo'),
     path('delete/<int:pk>', DeleteTodo.as_view())
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework import generics
 from .serializers import *
 from .models import *
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
 
 class ListTodo(generics.ListAPIView):
     queryset = Todo.objects.all()
@@ -21,19 +23,24 @@ class DetailTodo(generics.RetrieveUpdateAPIView):
     queryset = Todo.objects.all()
     serializer_class = ToDoSerializer
 
+
 class CreateTodo(generics.CreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = ToDoSerializer
+    renderer_classes = [TemplateHTMLRenderer]
     template_name = "create.html"
 
-    def post( self,request, *args, **kwargs):
-        todos = Todo.objects.all()
-        contexto = {
-            'caminho_para_bootstrap_css': 'https://bootswatch.com/morph/bootstrap.min.css',
-            'todos' : todos,
-        }
-        return render(request, self.template_name, contexto)
+    def get(self, request):
+        serializer = self.serializer_class()
+        return render(request, self.template_name, {'serializer': serializer})
 
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return redirect('tarefas:list-todo')  # Redirect to your to-do list view
+        else:
+            return render(request, self.template_name, {'serializer': serializer})
 class DeleteTodo(generics.DestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = ToDoSerializer
